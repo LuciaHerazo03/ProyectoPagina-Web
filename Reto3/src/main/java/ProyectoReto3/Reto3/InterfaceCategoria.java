446f435b35f1b0dfd30dff63c094b788f52b96ee
LuciaHerazo03 <94501302+LuciaHerazo03@users.noreply.github.com>
2021-11-22 21:28:57 -0500

    first commit

---
+++ Reto3/src/main/java/ProyectoReto3/Reto3/InterfaceCategoria.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProyectoReto3.Reto3;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+/**
+ *
+ * @author Lucia Herazo
+ */
+public interface InterfaceCategoria extends CrudRepository<Categoria,Integer> {
+    
+}
---
+++ Reto3/src/main/java/ProyectoReto3/Reto3/Reto3Application.java
@@ -0,0 +1,15 @@
+
+
+package ProyectoReto3.Reto3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
